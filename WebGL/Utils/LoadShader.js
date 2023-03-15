b7138d5becbe7c702d82b349dc87fd699b182a7e
CaiSir <352824779@qq.com>
2023-03-15 18:34:42 +0800

    图像的旋转移动

--- WebGL/Utils/LoadShader.js
+++ WebGL/Utils/LoadShader.js
@@ -69,10 +69,27 @@
             });
         }
 
+        function createProgram(gl, vsSource, fsSource) {
+            //创建程序对象
+            const program = gl.createProgram();
+            //建立着色对象
+            const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+            const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+            //把顶点着色对象装进程序对象中
+            gl.attachShader(program, vertexShader);
+            //把片元着色对象装进程序对象中
+            gl.attachShader(program, fragmentShader);
+            //连接webgl上下文对象和程序对象
+            gl.linkProgram(program);
+            return program
+          }
+          
+
 
 export 
 {
     initShaders,
     ScaleLinear,
-    LoadImg
+    LoadImg,
+    createProgram
 }
